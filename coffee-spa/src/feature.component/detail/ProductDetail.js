192f7611911b6e34dcc7a25d7d1bc0364123a1cc
chajuhui123 <chajuhui123@naver.com>
2023-06-17 01:42:52 +0900

    [feat] 쇼핑몰 SPA : 장바구니 아이템 선택에 따른 selectedOptions state 관리

--- coffee-spa/src/feature.component/detail/ProductDetail.js
+++ coffee-spa/src/feature.component/detail/ProductDetail.js
@@ -1,20 +1,33 @@
+import SelectedOptions from "./SelectedOption.js";
+
 function ProductDetail({ $target, initialState }) {
+  let isInitialized = false; // 화면 깜빡임 방지
+
   const $productDetail = document.createElement("div");
   $productDetail.className = "ProductDetail";
 
   $target.appendChild($productDetail);
+
+  let selectedOptions = null;
 
   this.state = initialState;
 
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
+
+    if (selectedOptions) {
+      selectedOptions.setState({
+        selectedOptions: this.state.selectedOptions,
+      });
+    }
   };
 
   this.render = () => {
     const { product } = this.state;
 
-    $productDetail.innerHTML = `
+    if (!isInitialized) {
+      $productDetail.innerHTML = `
       <img src="${product.imageUrl}">
       <div class="ProductDetail__info">
         <h2>${product.name}</h2>
@@ -39,9 +52,55 @@ function ProductDetail({ $target, initialState }) {
         <div class="ProductDetail__selectedOptions"></div>
       </div>
     `;
+
+      const selectedOptions = new SelectedOptions({
+        $target: $productDetail.querySelector(
+          ".ProductDetail__selectedOptions"
+        ),
+        initialState: {
+          product: this.state.product,
+          selectedOptions: this.state.selectedOptions,
+        },
+      });
+
+      isInitialized = true;
+    }
   };
 
   this.render();
+
+  $productDetail.addEventListener("change", (e) => {
+    if (e.target.tagName === "SELECT") {
+      const selectedOptionId = parseInt(e.target.value);
+      const { product, selectedOptions } = this.state;
+      // 상품의 옵션 데이터에서 현재 선택한 optionId가 존재하는지 탐색
+      const option = product.productOptions.find(
+        (option) => option.id === selectedOptionId
+      );
+      // 이미 선택한 상품인지 선택된 상품 데이터에서 탐색
+      const selectedOption = selectedOptions.find(
+        (selectedOption) => selectedOption.optionId === selectedOptionId
+      );
+
+      // 위 경우 모두 아닌 상황에서 selectedOptions에 현재 선택한 옵션 추가
+      if (option && !selectedOption) {
+        const nextSelectedOptions = [
+          ...selectedOptions,
+          {
+            productId: product.id,
+            optionId: option.id,
+            optionName: option.name,
+            optionPrice: option.price,
+            quantity: 1,
+          },
+        ];
+        this.setState({
+          ...this.state,
+          selectedOptions: nextSelectedOptions,
+        });
+      }
+    }
+  });
 }
 
 export default ProductDetail;
