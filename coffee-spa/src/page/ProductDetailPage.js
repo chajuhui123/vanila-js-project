94f07f582b84e5ec186fcdff0a284c008d94212f
chajuhui123 <chajuhui123@naver.com>
2023-06-16 23:57:47 +0900

    [feat] 쇼핑몰 SPA : Detail 페이지 api 연동 및 select UI 구성

--- coffee-spa/src/page/ProductDetailPage.js
+++ coffee-spa/src/page/ProductDetailPage.js
@@ -1,16 +1,48 @@
+import { api } from "../api/api.js";
+import ProductDetail from "../feature.component/detail/ProductDetail.js";
+
 function ProductDetailPage({ $target, productId }) {
   this.state = {
     productId,
+    product: null,
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
   };
 
   const $page = document.createElement("div");
   $page.className = "ProductDetailPage";
-
   $page.innerHTML = "<h1>상품 정보</h1>";
 
   this.render = () => {
-    $target.appendChild($page);
+    if (!this.state.product) {
+      $target.innerHTML = "Loading..";
+    } else {
+      $target.innerHTML = "";
+      $target.appendChild($page);
+      new ProductDetail({
+        $target: $page,
+        initialState: {
+          product: this.state.product,
+          // ProductDetail의 initialState에 선택된 상품들을 담아둘 selectedOptions 추가
+          selectedOptions: [],
+        },
+      });
+    }
   };
+
+  this.fetchProduct = async () => {
+    const { productId } = this.state;
+    const product = await api.fetGetProductItemById(productId);
+    this.setState({
+      ...this.state,
+      product,
+    });
+  };
+
+  this.fetchProduct();
 }
 
 export default ProductDetailPage;
